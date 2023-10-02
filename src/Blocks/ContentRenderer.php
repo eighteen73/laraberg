bef9e26dcac06b748ef15ee68d05ac40aeb221a7
Ed Jeavons <ed@orphans.co.uk>
2023-10-02 18:15:21 +0100

    Add renderIntro() method to display page snippets

--- src/Blocks/ContentRenderer.php
+++ src/Blocks/ContentRenderer.php
@@ -25,4 +25,28 @@ class ContentRenderer
 
         return $output;
     }
+
+    public function renderIntro(string $content, int $num_blocks = null): string
+    {
+        $output = '';
+        $blocks = $this->parser->parse($content);
+
+        // Render up to a `core/more` block if one exists, or else render blocks
+        $num_blocks = $num_blocks ?? config('laraberg.render.intro_blocks', 2);
+        foreach ($blocks as $block_key => $block) {
+            if ($block->blockName === 'core/more') {
+                $num_blocks = $block_key + 1;
+                break;
+            }
+        }
+
+        foreach ($blocks as $block_key => $block) {
+            if ($block_key >= $num_blocks) {
+                break;
+            }
+            $output .= $block->render();
+        }
+
+        return $output;
+    }
 }
--- src/Traits/RendersContent.php
+++ src/Traits/RendersContent.php
@@ -17,6 +17,14 @@ trait RendersContent
         return $renderer->render(is_string($content) ? $content : '');
     }
 
+    public function renderIntro(string $column = null, int $num_blocks = null): string
+    {
+        $column = $column ?: $this->getContentColumn();
+        $renderer = app(ContentRenderer::class);
+        $content = $this->$column;
+        return $renderer->renderIntro(is_string($content) ? $content : '', $num_blocks);
+    }
+
     protected function getContentColumn()
     {
         if (property_exists($this, 'contentColumn')) {
